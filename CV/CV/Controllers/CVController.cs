4396c517907fda2a84f5905dd8155d1a9ae5f05e
Mushfiqur-Rahman-Abir <mushfiqur.abir@gmail.com>
2022-09-22 16:43:15 +0600

    My First CV

---
+++ CV/CV/Controllers/CVController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Md. Musfiqur Rahman";
+            ViewData["Nationality"] = "Bangladeshi";
+            ViewBag.PermanentAddress = "Dinajpur, Bangladesh";
+            ViewData["Email"] = "mushfiqur.abir@gmail.com";
+            return View();
+            
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult CCA()
+        {
+            ViewBag.name1 = "1. Assistant Rover Mate, BAF SHAHEEN AIR ROVER";
+            ViewBag.name2 = "2. Event Manager, AIUB Computer Club";
+            ViewData["extra1"] = "3. CO-OP, AIUB Photography Club";
+            ViewData["extra2"] = "4. Member, AIUB Shomoy Club";
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+
+        public ActionResult Intro()
+        {
+            return View();
+        }
+
+    }
+}
