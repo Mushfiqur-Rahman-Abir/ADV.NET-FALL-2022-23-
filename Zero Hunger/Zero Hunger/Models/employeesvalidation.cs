5a107083c44e6fda5a255811c5a8c06d2581c48e
Mushfiqur-Rahman-Abir <mushfiqur.abir@gmail.com>
2022-11-06 01:50:48 +0600

    Zero Hunger

---
+++ Zero Hunger/Zero Hunger/Models/employeesvalidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using static Zero_Hunger.Models.restaurant;
+
+namespace Zero_Hunger.Models
+{
+
+    [MetadataType(typeof(employeeMetaData))]
+    public partial class employee
+    {
+        public class employeeMetaData
+        {
+            [DisplayName("Employee Name")]
+            public string empname { get; set; }
+
+            [DisplayName("Employee Address")]
+            public string address { get; set; }
+
+            [DisplayName("Employee Mobile no")]
+            public Nullable<int> mobile { get; set; }
+        }
+    }
+}
---
+++ Zero Hunger/Zero Hunger/Models/restaurentsvalidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Zero_Hunger.Models
+{
+
+    [MetadataType(typeof(restaurantMetaData))]
+    public partial class restaurant
+    {
+
+        public class restaurantMetaData
+        {
+            [DisplayName("Restaurant Name")]
+            public string resname { get; set; }
+
+            [DisplayName("Food Item")]
+            public string food { get; set; }
+
+            [DisplayName("Quantity(Kg/Pcs)")]
+            public string quantity { get; set; }
+
+            [DisplayName("Available")]
+            public string available { get; set; }
+
+
+
+        }
+
+
+
+    }
+}
