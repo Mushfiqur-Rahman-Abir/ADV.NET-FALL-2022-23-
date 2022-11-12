e89967d8420009c0411f8170e9e39fa0f6dea59e
Mushfiqur-Rahman-Abir <77951132+Mushfiqur-Rahman-Abir@users.noreply.github.com>
2022-11-12 16:26:38 +0600

    Files upload

---
+++ Zero_Hunger/Zero_Hunger/Controllers/showController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using Zero_Hunger.Models;
+
+namespace Zero_Hunger.Controllers
+{
+    public class showController : Controller
+    {
+
+        ZhungerEntities db = new ZhungerEntities();
+
+        // GET: ngo
+        public ActionResult Index()
+        {
+            var result = (from r in db.ngoes
+                          join c in db.Frestaurents on r.resid equals c.resname
+
+                          select new NgoAvail
+                          {
+                              id = r.id,
+                              resid = r.resid,
+                              employeeid = r.employeeid,
+                              sdate = r.sdate,
+                              edate = r.edate,
+                              available = c.available
+
+
+                          }).ToList();
+
+
+
+
+            return View(result);
+        }
+    }
+}
---
+++ Zero_Hunger/Zero_Hunger/Models/restaurent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Zero_Hunger.Models
+{
+    [MetadataType(typeof(FrestaurentMetaData))]
+    public partial class Frestaurent
+    {
+        
+       public class FrestaurentMetaData
+        {
+
+
+            [DisplayName("Restaurent Name")]
+            public string resname { get; set; }
+
+            [DisplayName("Restaurent Address")]
+            public string address { get; set; }
+            [DisplayName("Food Item")]
+            public string fooditem { get; set; }
+
+            [DisplayName("Availabile")]
+            public string available { get; set; }
+            public System.DateTime date { get; set; }
+
+
+
+        }
+      
+    }
+}
